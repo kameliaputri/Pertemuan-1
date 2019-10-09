fb1073a86473c4b85745b55fb4594667dda1c46c
kameliaputri <53284562+kameliaputri@users.noreply.github.com>
2019-10-09 11:28:54 +0700

    Add files via upload

---
+++ latihan 1.php
@@ -0,0 +1,32 @@
+<?php
+//echo "Hello world !";
+//echo "<h1>Halo Dunia! </h1>";
+
+//VARIABEL
+//diawali dengan tanda sama $nama
+//nama variabel tidak boleh diawali dengan angka
+//boleh mengandung angka
+//$nama = "Kamelia putri";
+//$pesan = "Halo, selamat datang <strong>nama</strong>";
+//echo $pesan;
+
+//Operator 
+//Aritmetika
+//+-*/%
+$a = 6;
+$b = 2;
+echo $a% $b;
+
+//CONCATENATION / CONCAT / PENGGABUNG STRING
+//.
+//$nama_depan = "Kamelia";
+//$nama_belakang = "Putri";
+//echo $nama_depan . " " . $ nama_belakang;
+
+//Assigment / penugasan 
+//$x = "nama";
+//$x = "";
+//$x = "kalian";
+//echo $x;
+
+?>
